68e84fb2a69865ab93cf4c171a3dc8007ab08296
SharminAkter3 <mssharmin4145@gmail.com>
2022-10-25 07:29:19 -0700

    added-courses-data

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const courses = require('./Data/courses.json')
 
 app.get('/', (req, res) => {
     res.send('index file running');
 });
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
 
 app.listen(port, () => {
     console.log('index file is running on port', port);
