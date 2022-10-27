ea610372f8df789da4fd4d4c34aef7028fecd57a
SharminAkter3 <mssharmin4145@gmail.com>
2022-10-26 21:58:25 -0700

    added-gitignore-file

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const courses = require('./Data/courses.json')
+const courses = require('./Data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('index file running');
@@ -13,7 +13,14 @@ app.get('/', (req, res) => {
 
 app.get('/courses', (req, res) => {
     res.send(courses);
+});
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const course = courses.find(c => c.id === id);
+    res.send(course);
 })
+
 
 app.listen(port, () => {
     console.log('index file is running on port', port);
